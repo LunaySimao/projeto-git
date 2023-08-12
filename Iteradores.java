4ddc06345fadebfb3972308c0bca82f4c5658a87
LunaySimao <80088146+LunaySimao@users.noreply.github.com>
2023-08-12 02:51:13 +0100

    java v1

---
+++ Iteradores.java
@@ -0,0 +1,16 @@
+package iteradores;
+
+/**
+ *
+ * @author Surface
+ */
+public class Iteradores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Iteradores");
+    }
+    
+}
